3d624901c8d64f655af1e867ed4b590244355807
nakahira <1021057927@qq.com>
2021-12-05 19:23:52 +0800

    remove redundant code

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,8 +1,1 @@
-require 'cocoapods-fix-xcframework-slice/command'
-
-
-module CocoapodsFixXcframeworkSlice
-  Pod::HooksManager.register('cocoapods-fix-xcframework-slice', :pre_install) do
-    load File.expand_path('../cocoapods-fix-xcframework-slice/cocoapods-fix-xcframework-slice.rb', __FILE__)
-  end
-end
+require 'cocoapods-fix-xcframework-slice/cocoapods-fix-xcframework-slice'
